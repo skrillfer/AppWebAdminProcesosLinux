027b587607e4868b2f2b87c540604c019cb99f1a
skrillfer <rluis4490@gmail.com>
2019-06-12 00:07:12 -0600

    lectura en tiempo real de archivos proc

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var express = require('express');
+const fs = require('fs');
+var watch = require('node-watch');
+require('log-timestamp');
+
 var app = express();
 
 app.get('/', function (req, res) {
@@ -8,3 +12,29 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+fs.watchFile('/proc/meminfo', { recursive: true }, function(evt, name) {
+  try {
+    console.log(info());
+  } catch (error) {
+    
+  }
+});
+
+function info() {
+  var info = {};
+  var data = fs.readFileSync('/proc/meminfo').toString();
+  data.split(/\n/g).forEach(function(line){
+      line = line.split(':');
+
+      // Ignore invalid lines, if any
+      if (line.length < 2) {
+          return;
+      }
+
+      // Remove parseInt call to make all values strings
+      info[line[0]] = parseInt(line[1].trim(), 10);
+  });
+
+  return info;
+}
